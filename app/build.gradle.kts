4d59b79f87b533ecb9700dc3740fda656457e0b1
munbonecci <munbonecciq@gmail.com>
2024-08-16 11:40:23 -0600

    kapt, dagger hilt, retrofit and moshi libraries added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
+    alias(libs.plugins.hilt.android.plugin)
 }
 
 android {
@@ -66,4 +68,13 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    //retrofit
+    implementation (libs.retrofit)
+    implementation (libs.converter.moshi)
+    //moshi
+    implementation (libs.moshi.kotlin)
+    implementation (libs.moshi.adapters)
+    // Dagger Hilt
+    implementation (libs.hilt.android)
+    kapt (libs.hilt.android.compiler)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.secrets) apply false
+    kotlin("kapt") version "1.8.10"
+    alias(libs.plugins.hilt.android.plugin) apply false
 }
