42949d14a151d0c945dfbd463b17e0cf90ce0662
munbonecci <munbonecciq@gmail.com>
2024-08-17 15:34:48 -0600

    Basic Room configuration added

---
+++ app/features/movies/src/main/java/com/munbonecci/movies/db/dao/MoviesDao.kt
@@ -0,0 +1,32 @@
+package com.munbonecci.movies.db.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.munbonecci.movies.db.entity.MovieEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MovieDao {
+
+    @Query("SELECT * FROM movies")
+    fun getAllMovies(): Flow<List<MovieEntity>>
+
+    @Query("SELECT * FROM movies WHERE id = :movieId")
+    fun getMovieById(movieId: Int): Flow<MovieEntity?>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(movies: List<MovieEntity>)
+
+    @Delete
+    suspend fun delete(movie: MovieEntity)
+
+    @Update
+    suspend fun update(movie: MovieEntity)
+
+    @Query("DELETE FROM movies")
+    suspend fun deleteAll()
+}
