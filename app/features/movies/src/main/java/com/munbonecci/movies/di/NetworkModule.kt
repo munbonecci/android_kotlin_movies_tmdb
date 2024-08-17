b022c0afe4d3f1f07dbaf805503ca937f365194b
munbonecci <munbonecciq@gmail.com>
2024-08-17 01:01:08 -0600

    Dependency injection to get retrofit instance and repository added

---
+++ app/features/movies/src/main/java/com/munbonecci/movies/di/NetworkModule.kt
@@ -0,0 +1,35 @@
+package com.munbonecci.movies.di
+
+import com.munbonecci.movies.data.api.MovieApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        val builder = OkHttpClient.Builder()
+        return builder.build()
+    }
+
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api.themoviedb.org/3/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Provides
+    fun provideMovieService(retrofit: Retrofit): MovieApi {
+        return retrofit.create(MovieApi::class.java)
+    }
+}
