b4b43c105595823ebb10110f5571d9ad7250f4e3
munbonecci <munbonecciq@gmail.com>
2024-08-17 01:02:41 -0600

    movie response and movie object added

---
+++ app/features/movies/src/main/java/com/munbonecci/movies/domain/models/Movie.kt
@@ -0,0 +1,22 @@
+package com.munbonecci.movies.domain.models
+
+import com.google.gson.annotations.SerializedName
+
+data class Movie(
+    var adult: Boolean? = null,
+    @SerializedName("backdrop_path")
+    var backdropPath: String? = null,
+    @SerializedName("genre_ids")
+    var genreIds: ArrayList<Int> = arrayListOf(),
+    var id: Int? = null,
+    @SerializedName("original_language") var originalLanguage: String? = null,
+    @SerializedName("original_title") var originalTitle: String? = null,
+    @SerializedName("overview") var overview: String? = null,
+    var popularity: Double? = null,
+    @SerializedName("poster_path") var posterPath: String? = null,
+    @SerializedName("release_date") var releaseDate: String? = null,
+    var title: String? = null,
+    var video: Boolean? = null,
+    @SerializedName("vote_average") var voteAverage: Double? = null,
+    @SerializedName("vote_count") var voteCount: Int? = null
+)
