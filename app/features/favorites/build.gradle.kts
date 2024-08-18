1026c600236f37ef5752457e37ed46d6c2201362
munbonecci <munbonecciq@gmail.com>
2024-08-17 19:12:57 -0600

    favorite module added

---
+++ app/features/favorites/build.gradle.kts
@@ -0,0 +1,85 @@
+plugins {
+    id("com.android.library")
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.secrets)
+    id("kotlin-kapt")
+    alias(libs.plugins.hilt.android.plugin)
+}
+
+android {
+    namespace = "com.munbonecci.favorites"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 34
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildFeatures {
+        buildConfig = true
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.2"
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    hilt {
+        enableAggregatingTask = true
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation (libs.androidx.lifecycle.viewmodel.ktx)
+    implementation (libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation (libs.androidx.material)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    //retrofit
+    implementation (libs.retrofit)
+    implementation (libs.converter.gson)
+    //okhttp
+    implementation (libs.okhttp)
+    //Gson
+    implementation (libs.gson)
+    // Dagger Hilt
+    implementation (libs.hilt.android)
+    kapt (libs.hilt.android.compiler)
+    //coil
+    implementation (libs.coil.kt.coil.compose)
+    //Navigation
+    implementation (libs.androidx.navigation.compose)
+    //Room
+    implementation (libs.androidx.room.runtime)
+    kapt (libs.androidx.room.compiler)
+    implementation (libs.androidx.room.ktx)
+}
